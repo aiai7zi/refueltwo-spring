f6aea5992b8e3051b7728bbfe98f57acbca610f8
谭吉 <99425549@qq.com>
2019-11-28 18:11:05 +0800

    ss

---
+++ refueltwo001-annotation01-configuration01/src/main/java/com/refueltwo/main/Refueltwo001Annotation01Configuration01Application.java
@@ -0,0 +1,13 @@
+package com.refueltwo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Refueltwo001Annotation01Configuration01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Refueltwo001Annotation01Configuration01Application.class, args);
+    }
+
+}
